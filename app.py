791c502856964a275f49ef32834a507bb802f803
ThaiNQ <110602193+thainq107@users.noreply.github.com>
2024-11-28 10:41:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 import os
 import torch
+import torch.nn as nn
 import streamlit as st
 from PIL import Image
 import torchvision.transforms as transforms
+
+class LeNetClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding='same')
+        self.avgpool1 = nn.AvgPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.avgpool2 = nn.AvgPool2d(kernel_size=2)
+        self.flatten = nn.Flatten()
+        self.fc_1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc_2 = nn.Linear(120, 84)
+        self.fc_3 = nn.Linear(84, num_classes)
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.avgpool1(outputs)
+        outputs = F.relu(outputs)
+        outputs = self.conv2(outputs)
+        outputs = self.avgpool2(outputs)
+        outputs = F.relu(outputs)
+        outputs = self.flatten(outputs)
+        outputs = self.fc_1(outputs)
+        outputs = self.fc_2(outputs)
+        outputs = self.fc_3(outputs)
+        return outputs
 
 @st.cache_resource
 def load_model(model_path, num_classes=10):
